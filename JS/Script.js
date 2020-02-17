b420250e46643a63ef8a3908bcaddd97fb0d6202
Daniel Martin Ochoa <56157528+dmartinochoa@users.noreply.github.com>
2020-02-17 22:33:43 +0100

    less reduntant code

--- JS/Script.js
+++ JS/Script.js
@@ -1,4 +1,6 @@
 let resultsList = document.querySelector('#results');
+let citySearch = document.querySelector('#cityTextArea');
+var count = 0;
 
 function enterSearch() {
     let key = event.keyCode;
@@ -29,15 +31,17 @@ function fetchData() {
 
 function addShow(response) {
     resultsList.innerHTML = ``;
-    var count = 0;
-    let citySearch = document.querySelector('#cityTextArea');
+    //this will check that it found values
     if (response.hasOwnProperty('_embedded')) {
         let evts = response._embedded.events;
         for (let i = 0; i < evts.length; i++) {
             let tourName = evts[i].name;
             let tourDate = evts[i].dates.start.localDate;
             let tourTime = evts[i].dates.start.localTime;
-            let tourTimeFormat = tourTime.substring(0, 5);
+            if (tourTime != undefined) {
+                //A veces localtime no devuelve nada asique formatear la fecha dara error
+                tourTime = tourTime.substring(0, 5);
+            }
             let venue = evts[i]._embedded.venues[0].name;
             let showCity = evts[i]._embedded.venues[0].city.name;
             let buyLink = evts[i].url;
@@ -47,43 +51,47 @@ function addShow(response) {
             if (city === "" || showCity.toLowerCase() === city) {
                 count++;
                 resultsList.innerHTML += `
-        <li class="resultLi">
-            <div class="row" id="listRow">
-                <div class="col-4" id="resultImg">
-                    <div class="imageContainer">
-                        <img class="bandImage" src="${imageLink}" alt="Artis Image">
-                    </div>
-                </div>
+                 <li class="resultLi">
+                     <div class="row" id="listRow">
+                        <div class="col-4 d-flex flex-wrap align-items-center" id="resultImg">
+                            <div class="imageContainer">
+                                    <img class="bandImage" src="${imageLink}" alt="Artis Image">
+                            </div>
+                        </div>
 
-                <div class="col-8" id="resultinfo">
-                    <div class="showInfo">
-                        <div class="nameAndDate">
-                            <span class="tourName">${tourName}</span>
-                            <span class="tourDate"> - ${tourDate}</span> 
-                        </div>
-                        <div class="details">
-                            <span class="tourTime">${tourTimeFormat} - </span>
-                            <span class="venue">${venue}</span>
-                            <br>
-                            <span class="showCity">${showCity}</span>
-                        </div>
-                        <div class="buyTickets">
-                            <a class="buyLink" href="${buyLink}">Buy Tickets</a>   
+                        <div class="col-8 d-flex " id="resultinfo">
+                            <div class="showInfo">
+                                <div class="nameAndDate">
+                                    <span class="tourName">${tourName}</span>
+                                    <span class="tourDate"> - ${tourDate}</span> 
+                                </div>
+                            <div class="details">
+                                <span class="tourTime">${tourTime} - </span>
+                                <span class="venue">${venue}</span>
+                                <br>
+                                <span class="showCity">${showCity}</span>
+                            </div>
+                            <div class="buyTickets">
+                            <a class="buyLink" href="${buyLink}">Buy Tickets <i class="fas fa-external-link-alt"></i> </i></a>   
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </li>
-        <hr class="hr">
-        `;
+            </li>
+            <hr class= "hr">`;
             }
         }
         citySearch.value = "";
     }
-    if (count != 1) {
-        resultsList.prepend(count + " concerts found");
-    } else {
-        resultsList.prepend(count + " concert found");
-    }
+    concertCounter();
+    count = 0;
+}
 
+function concertCounter() {
+    let showCount = document.querySelector("#counter");
+    if (count != 1) {
+        showCount.innerHTML = `${count} concerts found`
+    } else {
+        showCount.innerHTML = `${count} concert found`
+    }
 }
